e925417fc3f440981df188e16bee4deb2996d098
shukufabayramzada <shukufabayramzada@gmail.com>
2024-07-20 19:26:35 +0400

    update

--- ES6_basic/eslint.config.mjs
+++ ES6_basic/eslint.config.mjs
@@ -3,7 +3,6 @@ import pluginJs from "@eslint/js";
 
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
 ];
