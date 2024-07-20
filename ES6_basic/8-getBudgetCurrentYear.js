6aba80350b49ed3a36402f45e373d35eeae9c781
shukufabayramzada <shukufabayramzada@gmail.com>
2024-07-20 14:59:37 +0400

    object creation shorthand

---
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const currentYear = getCurrentYear();
+
+  const budget = {
+    [`income-${currentYear}`]: income,
+    [`gdp-${currentYear}`]: gdp,
+    [`capita-${currentYear}`]: capita,
+  };
+
+  return budget;
+}
