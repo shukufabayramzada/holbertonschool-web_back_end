6daf499e863d71290c3d92aa19ddd5f480120d0b
shukufabayramzada <shukufabayramzada@gmail.com>
2024-07-20 17:37:12 +0400

    solved eslint problem

--- ES6_basic/.eslintrc.js
+++ ES6_basic/.eslintrc.js
@@ -1,32 +1,28 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
-    node: true,
+    browser: false,
+    es6: true,
     jest: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ['airbnb-base', 'plugin:jest/all'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'jest'],
+  plugins: ['jest'],
   rules: {
-    'no-unused-vars': 'warn',
     'no-console': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: true,
-        optionalDependencies: false,
-        peerDependencies: false,
-        packageDir: ['./', './src'],
-      },
-    ],
+    'no-shadow': 'off',
+    'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
   },
+  overrides: [
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    },
+  ],
 };
