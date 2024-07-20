edd9cfe5bab4c04b74608273287812b946a8466f
shukufabayramzada <shukufabayramzada@gmail.com>
2024-07-20 19:20:24 +0400

    update

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,4 +1,7 @@
-/* eslint-disable */
-export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+export default function getSumOfHoods(
+  initialNumber,
+  expansion1989 = 89,
+  expansion2019 = 19
+) {
   return initialNumber + expansion1989 + expansion2019;
 }
