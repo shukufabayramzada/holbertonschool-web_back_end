2a1379d9cc385c3ab73f649181c8a5382be22d05
shukufabayramzada <shukufabayramzada@gmail.com>
2024-07-21 15:36:56 +0400

    eslint fixes

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,9 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (
-      this.constructor !== Building &&
-      typeof this.evacuationWarningMessage !== 'function'
-    ) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error(
         'Class extending Building must override evacuationWarningMessage'
       );
