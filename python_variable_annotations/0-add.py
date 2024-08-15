9f7f9164d247b58fde17bcaa4c1530204887553c
shukufabayramzada <shukufabayramzada@gmail.com>
2024-08-15 13:43:18 +0400

    initialization with type annotations

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,14 @@
-#!/usr/bin/python3
-def add(a: float, b:float) -> float:
-    return  a + b
+#!/usr/bin/env python3
+
+'''
+Module defines type-annotated function add that
+takes a float a and a float b as arguments and returns their sum as a float.
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    Function takes two floats and return their sum
+    as float.
+    '''
+    return a + b
