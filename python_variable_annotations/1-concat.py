b8d160def6f949789eaf8434ef7d0860d22c15c1
shukufabayramzada <shukufabayramzada@gmail.com>
2024-08-15 13:47:51 +0400

    string concat with type annotations

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+'''
+Module defines a type-annotated function concat that takes
+a string str1 and a string str2 as arguments and returns a concatenated string
+'''
+
+
+def concat(a: str, b: str) -> str:
+    '''
+    Function that takes two string and concat them
+    '''
+    return a + b
