f87e3b0a460d9c706529c5107d53753355674083
shukufabayramzada <shukufabayramzada@gmail.com>
2024-08-15 16:45:47 +0400

    multiply by float

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+'''
+Module defines a type-annotated function make_multiplier
+that takes a float multiplier
+as argument and returns a function that multiplies a float by multiplier.
+'''
+from typing import Callable
+
+    
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''
+    Declaration
+    '''
+    def multiplier_function(value: float) -> float:
+        return value * multiplier
+    return multiplier_function
