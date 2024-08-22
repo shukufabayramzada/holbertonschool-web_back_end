04a05297343f9fa402747ece59f72a323adf8295
shukufabayramzada <shukufabayramzada@gmail.com>
2024-08-22 13:20:53 +0400

    error resolved maybe

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,7 +10,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[int, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     '''
     Async Generator
     '''
