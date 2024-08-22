fb588b34d91ef860f409b977a469a523ca79cf2e
shukufabayramzada <shukufabayramzada@gmail.com>
2024-08-22 15:32:41 +0400

    may resolve the error

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,9 +2,10 @@
 '''
 Module defines simple helper function
 '''
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
-    start_index = (page - 1) ** page_size
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
