9c3e5cbec0e684ebf072282b304a73bfd35f456f
shukufabayramzada <shukufabayramzada@gmail.com>
2024-08-22 13:31:04 +0400

    type error fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,7 +10,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]: # type: ignore
     """Function for asynchronous generator."""
     for _ in range(10):
         await asyncio.sleep(1)
