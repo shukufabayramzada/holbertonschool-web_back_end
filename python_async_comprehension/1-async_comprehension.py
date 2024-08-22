57b7760d2cc9c87d9296d4a623ea523632f66db2
shukufabayramzada <shukufabayramzada@gmail.com>
2024-08-22 13:43:43 +0400

    async comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,10 +3,11 @@
 Module defines a coroutine called async_comprehension
 that takes no arguments.
 '''
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     '''
     Function for async comprehension
     '''
