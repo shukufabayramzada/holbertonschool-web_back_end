e61d33df4d075b63fda1aaba642430e99061e717
shukufabayramzada <shukufabayramzada@gmail.com>
2024-08-22 15:34:33 +0400

    may resolve the error

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,6 +6,10 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''
+    Function that returns a tuple of size two
+    containing a start index and an end index
+    '''
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
