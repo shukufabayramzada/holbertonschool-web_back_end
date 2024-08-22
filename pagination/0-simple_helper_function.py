a5c3449538515d7011aab07c881bcf6e6a12f2be
shukufabayramzada <shukufabayramzada@gmail.com>
2024-08-22 15:26:03 +0400

    simple pagination

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+Module defines simple helper function
+'''
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    start_index = (page - 1) ** page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
