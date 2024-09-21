1f7ff95aca977c0f96a95c88a16e95bf461b1aac
shukufabayramzada <shukufabayramzada@gmail.com>
2024-09-21 15:22:52 +0400

    using process stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+// 1-stdin.js
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (input) => {
+  const name = input.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+
+  process.stdout.write('This important software is now closing\n');
+  process.stdin.end();
+});
