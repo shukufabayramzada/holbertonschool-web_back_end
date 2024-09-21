33184fc0c9d0e9bf9f1e920af78fac80ec4c04b1
shukufabayramzada <shukufabayramzada@gmail.com>
2024-09-21 16:12:22 +0400

    please be the end

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -2,34 +2,33 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter((line) => line.trim().length > 0);
-    lines.shift();
+    const lines = fs.readFileSync(path, { encoding: 'utf8' }).split(/\r?\n/);
+    let countStudents = 0;
+    const fields = {};
 
-    const studentsByField = {};
-
-    lines.forEach((line) => {
-      const [firstname, , , field] = line.split(',');
-      if (!studentsByField[field]) {
-        studentsByField[field] = [];
-      }
-      studentsByField[field].push(firstname);
-    });
-
-    const totalStudents = lines.length;
-    console.log(`Number of students: ${totalStudents}`);
-
-    for (const field in studentsByField) {
-      if (Object.prototype.hasOwnProperty.call(studentsByField, field)) {
-        const students = studentsByField[field];
-        console.log(
-          `Number of students in ${field}: ${
-            students.length
-          }. List: ${students.join(', ')}`
-        );
+    for (let i = 1; i < lines.length; i += 1) {
+      const line = lines[i].trim();
+      if (line !== '') {
+        countStudents += 1;
+        const [fname, , , field] = line.split(',');
+        if (!fields[field]) {
+          fields[field] = {
+            countField: 1,
+            students: [fname],
+          };
+        } else {
+          fields[field].countField += 1;
+          fields[field].students.push(fname);
+        }
       }
     }
-  } catch (error) {
+    console.log(`Number of students: ${countStudents}`);
+    for (const field of Object.keys(fields)) {
+      const n = fields[field].countField;
+      const names = fields[field].students.join(', ');
+      console.log(`Number of students in ${field}: ${n}. List: ${names}`);
+    }
+  } catch (err) {
     throw new Error('Cannot load the database');
   }
 }
