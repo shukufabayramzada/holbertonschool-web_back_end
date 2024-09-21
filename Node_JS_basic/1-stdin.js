9744616cef5af398b85acd673209280baa783ff6
shukufabayramzada <shukufabayramzada@gmail.com>
2024-09-21 15:30:06 +0400

    may it will true

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,15 @@
 // 1-stdin.js
+const os = require('os');
 
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write(
+  `Welcome to Holberton School, what is your name?${os.EOL}`
+);
 
 process.stdin.on('data', (input) => {
   const name = input.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write(`Your name is: ${name}${os.EOL}`);
 
-  process.stdout.write('This important software is now closing\n');
+  process.stdout.write(`This important software is now closing${os.EOL}`);
+
   process.exit();
 });
